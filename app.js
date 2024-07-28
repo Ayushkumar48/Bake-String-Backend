b9a3e6084c125cd7e977c7bcd9f3d3fa3c37c38f
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-07-28 22:43:57 +0530

    .

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
