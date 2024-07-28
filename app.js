e4271761d28b9989855a392ab636385cc4abdefb
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-07-28 23:56:19 +0530

    .

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors({ origin: "https://bakestring.tech", credentials: true }));
+app.use(cors({ origin: "https://bakestring.tech" }));
 
 const uri =
   "mongodb+srv://ayushsuperstar48:ayush16@bake-string.meeaaiw.mongodb.net/?retryWrites=true&w=majority&appName=bake-string";
